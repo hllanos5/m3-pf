17398345f2a7b7cda79fc152a00bf8104ea7cdc0
Hans Llanos <hans.llanos@gmail.com>
2024-08-28 14:53:34 -0500

    avance de cards inferiores

--- practica/src/components/LadoDerecho.jsx
+++ practica/src/components/LadoDerecho.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card } from 'primereact/card';
 import { Image } from 'primereact/image';
-import { Button } from 'primereact/button';
+import { Slider } from "primereact/slider";
 
 export function LadoDerecho() {
 
@@ -11,6 +11,7 @@ const footer = (
         <div className='minimo'>11°C</div>
     </>
 );
+
   return (
     <>
         <Card className='unidad-medida'>
@@ -19,21 +20,61 @@ const footer = (
         </Card>
         <div className='panel-clima'> 
             <Card footer={footer} header='Tomorrow'>                
-                <Image src="./images/HeavyRain.png" alt="Image" width="80" />                   
+                <Image src="./images/HeavyRain.png" alt="Image" width="45" />                   
             </Card>  
             <Card footer={footer} header='Tomorrow'>                
-                <Image src="./images/HeavyRain.png" alt="Image" width="80" />                   
+                <Image src="./images/HeavyRain.png" alt="Image" width="45" />                   
             </Card>
             <Card footer={footer} header='Tomorrow'>                
-                <Image src="./images/HeavyRain.png" alt="Image" width="80" />                   
+                <Image src="./images/HeavyRain.png" alt="Image" width="45" />                   
             </Card>
             <Card footer={footer} header='Tomorrow'>                
-                <Image src="./images/HeavyRain.png" alt="Image" width="80" />                   
+                <Image src="./images/HeavyRain.png" alt="Image" width="45" />                   
             </Card>
             <Card footer={footer} header='Tomorrow'>                
-                <Image src="./images/HeavyRain.png" alt="Image" width="80" />                   
+                <Image src="./images/HeavyRain.png" alt="Image" width="45" />                   
             </Card>     
+        </div>        
+        <h1 className='highlight'>Today's Highlights</h1>
+        <div className='panel-highlight'>
+            <Card header='Wind status'>                
+                <div className='contenido'>
+                    <span className='grande'>7</span>
+                    <span className='pequeno'>mph</span>
+                </div>
+                <div className='pie'>
+                    <i className="pi pi-send"></i>WSW
+                </div>
+            </Card>
+            <Card  header='Humidity'>   
+                <div className='contenido'>             
+                    <span className='grande'>84</span>
+                    <span className='pequeno'>%</span>
+                </div>
+                <div className='pie'>
+                    <div className='porcentaje'>
+                        <span>0</span>
+                        <span>50</span>
+                        <span>100</span>
+                    </div>           
+                    <Slider value='84' className="w-14rem" />
+                    <div className='porcentaje-footer'>%</div>
+                </div>              
+            </Card>
+            <Card header='Visibility'>                
+                <div className='contenido'>
+                    <span className='grande'>6,4</span>
+                    <span className='pequeno'>miles</span>
+                </div>
+            </Card>
+            <Card header='Air Pressure'>                
+                <div className='contenido'>
+                    <span className='grande'>998</span>
+                    <span className='pequeno'>mb</span>
+                </div>
+            </Card>
         </div>
+        
     </>
   )
 }
